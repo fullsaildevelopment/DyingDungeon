706e993f71c0ee5b595f4b27bd451855653b2fab
Lane Langford <dascoundrel64@gmail.com>
2019-12-10 15:17:02 -0500

    Test Push

--- DyingDungeon/DyingDungeon/Skills.h
+++ DyingDungeon/DyingDungeon/Skills.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "EnemyComponent.h"
 class Skills
 {
 //public and private variables
@@ -10,9 +10,15 @@ private:
 	float m_damage;
 	//how much mana it will cost the caster
 	float m_mpCost;
+//public and private functions
 public:
+	//constructor, give how much damage you want the skill to do and how much mana you want it to cost 
 	Skills(float dps, float mana);
-	void Use();
+	~Skills();
+	//get how much mana the skill cost
+	float GetManaCost();
+	//use the skill
+	void Use(EnemyComponent& target);
 private:
 
 };
