2a1c43b81aba9e9b14f76744f0ded1f5195819e5
Tristen Maturski <maturski62@gmail.com>
2019-12-12 11:02:14 -0500

    Added Logic in Battle Instance
    Battle Instance now has BATTLE_STATE.
    Checks if units are alive or dead and removes characters from the battle queue if the character is dead.

--- DyingDungeon/DyingDungeon/src/BattleInstance.h
+++ DyingDungeon/DyingDungeon/src/BattleInstance.h
@@ -4,6 +4,12 @@
 #include <vector>
 #include <time.h>
 
+enum BATTLE_STATE
+{
+	IN_BATTLE,
+	BATTLE_OVER
+};
+
 class BattleInstance : public Odyssey::Component
 {
 	CLASS_DECLARATION(BattleInstance);
@@ -12,19 +18,44 @@ public: // Constructors
 	BattleInstance(std::vector<std::shared_ptr<Odyssey::GameObject>> _playerTeam, std::vector<std::shared_ptr<Odyssey::GameObject>> _enemyTeam);
 
 public: // Functions
-	virtual void initialize(Odyssey::GameObject* Parent);
+
+	// Necessary
+	virtual void initialize(Odyssey::GameObject* _parent);
 	virtual void update(double deltaTime);
 
+	//Getters
+	int GetBattleState(){ return mBattleState; }
+
+	//Setters
+
+
 private: // Varibales
-	std::vector<std::shared_ptr<Odyssey::GameObject>> allCharacters; // allCharacters will hold all of the game objects that are in the current battle
-	std::vector<std::shared_ptr<Odyssey::GameObject>> playerTeam; // playerTeam will hold the gameObjects associated with the Player's team
-	std::vector<std::shared_ptr<Odyssey::GameObject>> enemyTeam; // enemyTeam will hold the gameObjects associated with the Overlord's team
 
-	std::queue<std::shared_ptr<Odyssey::GameObject>> battleQueue; // Battle Queue that will hodl the order in which players can attack
+	// Vectors
+	std::vector<std::shared_ptr<Odyssey::GameObject>> mAllCharacters; // allCharacters will hold all of the game objects that are in the current battle
+	std::vector<std::shared_ptr<Odyssey::GameObject>> mPlayerTeam; // playerTeam will hold the gameObjects associated with the Player's team
+	std::vector<std::shared_ptr<Odyssey::GameObject>> mEnemyTeam; // enemyTeam will hold the gameObjects associated with the Overlord's team
 
-	std::shared_ptr<Odyssey::GameObject> pCurrentCharacter; // This will hold the current player who's turn it is
+	// Queues
+	std::queue<std::shared_ptr<Odyssey::GameObject>> mBattleQueue; // Battle Queue that will hodl the order in which players can attack
+
+	// GameObjects
+	std::shared_ptr<Odyssey::GameObject> mCurrentCharacter; // This will hold the current player who's turn it is
+
+	// Ints
+
+
+	// Floats
+
+
+	// Bools
+	bool mIsBattleOver;
+
+	// Battle State
+	BATTLE_STATE mBattleState;
 
 private: // Functions
 	void GenerateBattleQueue(); // This will generate the battle queue for the character turn orders
+	bool IsTeamAlive(std::vector<std::shared_ptr<Odyssey::GameObject>> _teamToCheck); // This will check to see if at least one character from the passed in team is alive
 };
 
