fed6eda00c0d016b648d45f186b2eaf23705c983
Tristen Maturski <maturski62@gmail.com>
2020-01-24 10:25:27 -0500

    All doors do animation

--- DyingDungeon/DyingDungeon/src/TowerSelectController.h
+++ DyingDungeon/DyingDungeon/src/TowerSelectController.h
@@ -17,21 +17,44 @@ public:
 private:
 	Odyssey::Application* mApplication;
 
-	// Bools
-	bool mDoorIsClosed = true;
-	bool mDoOpenDoorAnimation = false;
-	bool mDoCloseDoorAnimation = false;
+	struct Door
+	{
+		// The door image
+		Odyssey::Sprite2D* doorImage;
 
-	// Ints
-	int mDoorImageIndex = 1;
+		// Bools
+		bool mDoorIsClosed = true;
+		bool mDoOpenDoorAnimation = false;
+		bool mDoCloseDoorAnimation = false;
 
-	// Double
-	double totalTime = 0.0f;
+		// Ints
+		int mDoorImageIndex = 1;
+
+		// Double
+		double totalTime = 0.0f;
+	};
+
+	// Keep a list of the doors
+	std::vector<Door> mDoorList;
+
 
 private: //Functions
 	void GoToTeamSelection();
-	void ChangeDoorState();
 
-	void SetNextDoorImage(Odyssey::Sprite2D* _doorImageToChange);
+	// Change the door states on mouse enter and exit events
+	void ChangeDoor1State();
+	void ChangeDoor2State();
+	void ChangeDoor3State();
+	void ChangeDoor4State();
+	void ChangeDoor5State();
+
+	// Door Animation functions
+	void Door1Animation(double _deltaTime);
+	void Door2Animation(double _deltaTime);
+	void Door3Animation(double _deltaTime);
+	void Door4Animation(double _deltaTime);
+	void Door5Animation(double _deltaTime);
+
+	void SetNextDoorImage(Door _doorToChange);
 };
 
