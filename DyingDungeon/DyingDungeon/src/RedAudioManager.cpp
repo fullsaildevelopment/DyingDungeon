2ca47e9e413b8db3586f3cb4397a4fd4f999c84d
Jeffrey Ouma <JWOuma@student.fullsail.edu>
2020-02-10 16:34:10 -0500

    Organized audio files into directories

--- DyingDungeon/DyingDungeon/src/RedAudioManager.cpp
+++ DyingDungeon/DyingDungeon/src/RedAudioManager.cpp
@@ -31,7 +31,7 @@ RedAudioManager::RedAudioManager()
 	m_volume[2] = 1000;
 	m_volume[3] = 1000;
 	m_muted = false;
-	m_default_audio = new RedAudio("assets/audio/wheres_the_lamb_sauce.mp3", "DEFAULT");
+	m_default_audio = new RedAudio("assets/audio/SFX/wheres_the_lamb_sauce.mp3", "DEFAULT");
 	m_default_audio->Open();
 }
 
